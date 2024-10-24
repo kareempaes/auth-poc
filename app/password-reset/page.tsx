a0806230dc7897269f69dafbee72f722c2e2cdb0
Kareem Paes <kareemjpaes@gmail.com>
2024-10-24 09:30:15 -0600

    update file name

---
+++ app/password-reset/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Home() {
+
+  return (
+    <div className="grid grid-cols-2 items-center justify-items-center min-h-screen min-w-screen">
+      This is a test
+    </div>
+  );
+}
