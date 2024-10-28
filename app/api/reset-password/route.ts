ff0bfc4ba8da78b09a314bc74194f04273aeaf52
Kareem Paes <kareemjpaes@gmail.com>
2024-10-28 08:16:52 -0600

    update password

--- app/api/reset-password/route.ts
+++ app/api/reset-password/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: NextRequest) {
   const { email } = (await req.json()) as EmailFormData;     
   
   sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
-  const templateId = process.env.SENDGRID_RESET_PASSWORD_ID as string;
+  const templateId = process.env.SENDGRID_RESET_PASSWORD_TEMPLATE_ID as string;
   const from = process.env.SENDGRID_SENDER as string;
   
   const msg = {
--- app/password-update/page.tsx
+++ app/password-update/page.tsx
@@ -2,6 +2,8 @@
 import { Card, CardContent, TextField, Typography, Button, SvgIcon } from '@mui/material';
 import Image from 'next/image';
 import { makeStyles } from '@mui/styles';
+import { submitNewPassword } from "supertokens-web-js/recipe/emailpassword";
+import { useState } from 'react';
 
 
 const useStyles = makeStyles(() => ({
@@ -17,7 +19,35 @@ const useStyles = makeStyles(() => ({
   }))
 
 export default function PasswordUpdate () {
+    const [password, setPassword] = useState("");
     const classes = useStyles()
+
+
+    const submit = async (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
+        e.preventDefault();
+        
+        try {
+            const {status} = await submitNewPassword({
+                formFields: [{id: "password", value: password}],
+            });
+
+            if (status === "OK") {
+                window.alert("Success");
+            }
+            
+            if (status === "RESET_PASSWORD_INVALID_TOKEN_ERROR") {
+                window.alert("Invalid token");
+            }
+
+            if (status === "FIELD_ERROR") {
+                window.alert("Token theft detected");
+            }
+        } catch (e) {
+            console.log(e);
+            window.alert("An error occurred");
+        };
+    };
+
     return (
         <div className="grid grid-cols-2 items-center justify-items-center min-h-screen min-w-screen">
         <div className="w-full h-full bg-[url('/images/background.png')] bg-cover pl-8 pt-4">
@@ -28,7 +58,9 @@ export default function PasswordUpdate () {
                 <CardContent className='flex flex-col item-center p-0'>
                     <Typography variant='h6' align='center' className='mb-4' fontWeight={600} >Change your password</Typography>
                     <Typography variant='body1' align='center' className='mb-4' fontWeight={400}>Login to the Ambyint Platform</Typography>
-                    <TextField type='password' label='Password' id="outlined-password-input" className='mb-4' placeholder='Enter Password'></TextField>
+                    <TextField type='password' label='Password' id="outlined-password-input" className='mb-4' placeholder='Enter Password' onChange={
+                        (e) => setPassword(e.currentTarget.value)
+                    }></TextField>
                     <TextField type='password' label='Re-enter Password' id="outlined-password-input" className='mb-4' placeholder='Re-enter Password'></TextField>
                 </CardContent>
                 <div className={classes.borderOne}>
@@ -42,7 +74,7 @@ export default function PasswordUpdate () {
                     <Typography variant='body2' color='#737A8E'>No more than 2 identical characters in a row (e.g. &apos;aaa&apos; not allowed)</Typography>
                 </CardContent>
                 </div>
-                <Button variant='contained' disabled className='bg-[#36454F] w-full h-[62px] mb-4 normal-case'>Reset Password</Button>
+                <Button variant='contained' disabled className='bg-[#36454F] w-full h-[62px] mb-4 normal-case' onClick={e => submit(e)}>Reset Password</Button>
                 <Typography variant='body2' align='center' fontWeight={500}>Back to Login</Typography>
                 <SvgIcon></SvgIcon>
             </Card>
